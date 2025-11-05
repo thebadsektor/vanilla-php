fbada83f9ba3d6dcbc34e0f766f6c95350fc9e89
thebadsektor <gerald.villaran@lspu.edu.ph>
2025-11-05 10:03:25 +0800

    edit init.php

--- includes/init.php
+++ includes/init.php
@@ -2,24 +2,24 @@
     ob_start();
     session_start();
     //  *************** For PostgreSQL
-    $dsn = "pgsql:host=localhost;dbname=webmap302;port=5432";
-    $opt = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false
-    ];
-    $pdo = new PDO($dsn, 'postgres', '123456', $opt);
+    // $dsn = "pgsql:host=localhost;dbname=webmap302;port=5432";
+    // $opt = [
+    //     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    //     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //     PDO::ATTR_EMULATE_PREPARES   => false
+    // ];
+    // $pdo = new PDO($dsn, 'postgres', '123456', $opt);
     //  *************** For MySQL
-    //    $dsn = "mysql:host=localhost;dbname=login_course;port=3306;charset=utf8";
-    //    $opt = [
-    //        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    //        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    //        PDO::ATTR_EMULATE_PREPARES   => false
-    //    ];
-    //    $pdo = new PDO($dsn, $user, $pass, $opt);
+       $dsn = "mysql:host=localhost;dbname=big_sales_db;port=3306;charset=utf8";
+       $opt = [
+           PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+           PDO::ATTR_EMULATE_PREPARES   => false
+       ];
+       $pdo = new PDO($dsn, 'root', '', $opt);
     
-    $root_directory = "webmap302";
-    $from_email = "admin@imgenv.com";
-    $reply_email = "admin@imgenv.com";
+    $root_directory = "big_sales";
+    $from_email = "admin@big_sales.com";
+    $reply_email = "admin@big_sales.com";
     include "php_functions.php";
 ?>
